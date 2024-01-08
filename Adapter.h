b69924dc9277025c387a4be8d98a0a067fb1cd0e
Oles <oles1stepanov@gmail.com>
2024-01-08 16:25:20 +0200

    Add adapter

---
+++ Adapter.h
@@ -0,0 +1,11 @@
+#ifndef ADAPTER_H
+#define ADAPTER_H
+
+#include <iostream>
+
+void runAdapter()
+{
+
+}
+
+#endif // ADAPTER_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <QCoreApplication>
 
+#include "Adapter.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    runAdapter();
 
     return a.exec();
 }
