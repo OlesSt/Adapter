7a9945d2f6462ad37c648438f461bc876e7fd56f
Oles <oles1stepanov@gmail.com>
2024-01-08 17:27:35 +0200

    Add adapter

--- Adapter.h
+++ Adapter.h
@@ -3,9 +3,83 @@
 
 #include <iostream>
 
+class Scales
+{
+public:
+    Scales(){}
+    virtual ~Scales(){}
+    virtual float *getWeight() = 0;
+};
+
+class PoundScales : public Scales
+{
+private:
+    float *currentWeight;
+
+public:
+    PoundScales(float *cw) : currentWeight(cw) {}
+    ~PoundScales() override
+    {
+        currentWeight = nullptr;
+    }
+    virtual float* getWeight() override
+    {
+        return currentWeight;
+    }
+};
+
+class KiloScales : public Scales
+{
+private:
+    float *currentWeight;
+
+public:
+    KiloScales(float *cw) : currentWeight(cw){}
+    ~KiloScales() override
+    {
+        currentWeight = nullptr;
+    }
+    virtual float *getWeight() override
+    {
+        return currentWeight;
+    }
+};
+
+class Adapter : public Scales
+{
+public:
+    Adapter(PoundScales* pund) : pundWeight(pund), currentWeight(0) {}
+    ~Adapter()
+    {
+//        delete pundWeight;
+//        delete currentWeight;
+    }
+
+    virtual float *getWeight() override
+    {
+        currentWeight = pundWeight->getWeight();
+        *currentWeight = *currentWeight * 0.453;
+        return currentWeight;
+    }
+
+private:
+    PoundScales *pundWeight;
+    float *currentWeight;
+};
+
 void runAdapter()
 {
+    float kg {55.f};
+    float pnd {55.f};
 
+    Scales *kgScales = new KiloScales(&kg);
+    Scales *pnScales = new Adapter(new PoundScales(&pnd));
+
+    std::cout << *kgScales->getWeight() << std::endl;
+    std::cout << *pnScales->getWeight() << std::endl;
+
+    delete kgScales;
+    delete pnScales;
 }
 
 #endif // ADAPTER_H
